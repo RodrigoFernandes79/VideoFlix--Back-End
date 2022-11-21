549ad121f65502ff0449efecf2a21fdc51f79bbd
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-11-21 19:39:08 -0300

    Finalizando a configuração do JWT no spring security

--- src/main/java/com/rodrigohf/videoFlix/config/SecurityConfig.java
+++ src/main/java/com/rodrigohf/videoFlix/config/SecurityConfig.java
@@ -8,10 +8,15 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.rodrigohf.videoFlix.services.security.UsuarioService;
+import com.rodrigohf.videoFlix.services.security.jwt.JwtAuthFilter;
+import com.rodrigohf.videoFlix.services.security.jwt.JwtService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
@@ -20,12 +25,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@Autowired
+	private JwtService jwtService;
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		
 		return new BCryptPasswordEncoder();
 	}			
 		
+	@Bean
+	public OncePerRequestFilter jwtFilter() {
+		return new JwtAuthFilter(jwtService, usuarioService);
+	}
 		
 
 	@Override
@@ -57,7 +69,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.permitAll()
 		.anyRequest().authenticated()
 		.and()
-		.httpBasic();
+			 .sessionManagement()
+			 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			 .and()
+			 .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+		
 	
 	}
 	
