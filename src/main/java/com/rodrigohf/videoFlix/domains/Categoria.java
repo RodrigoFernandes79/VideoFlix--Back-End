e052950053b349ea5b5980841830116b14ed7f0f
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-12-01 20:11:44 -0300

    Criando Banco de dados no AWS RDS em ambiente de Produção

--- src/main/java/com/rodrigohf/videoFlix/domains/Categoria.java
+++ src/main/java/com/rodrigohf/videoFlix/domains/Categoria.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -22,6 +23,7 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @Entity
+@Table(name="Categoria")
 public class Categoria {
 	
 	
--- src/main/java/com/rodrigohf/videoFlix/domains/Video.java
+++ src/main/java/com/rodrigohf/videoFlix/domains/Video.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.URL;
@@ -21,6 +22,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name="Video")
 public class Video {
 	
 	@Id
